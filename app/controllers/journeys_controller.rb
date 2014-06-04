38c079a1771acb593e2a22b833db67fc777d6098
Josh Fishbein <Josh@Joshs-MacBook-Pro.local>
2014-06-04 11:08:53 -0400

    Allow stop markers to pass data to form and make sidebar stateful

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -17,13 +17,10 @@ class JourneysController < ApplicationController
   def show
     @journey = Journey.find(params[:id])
 
-    # @json = @journey.stops.each do |stop|
-
-    # end
     @hash = Gmaps4rails.build_markers(@journey.stops) do |stop, marker|
       marker.lat stop.latitude.to_s
       marker.lng stop.longitude.to_s
-      marker.infowindow stop.title
+      marker.infowindow "#{stop.title} <br/> See content"
     end
 
   end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -2,13 +2,15 @@ class Stop < ActiveRecord::Base
 
   belongs_to :journey
 
-  geocoded_by :location
-  before_validation :geocode
+  # geocoded_by :location
+  # before_validation :geocode
 
   validates :title, presence: true, length: { maximum: 60 }, uniqueness: { scope: :journey }
-  validates :location, presence: true, length: { maximum: 128 }, uniqueness: { scope: :journey }
-  validates :latitude, presence: true, uniqueness: { scope: :journey }
-  validates :longitude, presence: true, uniqueness: { scope: :journey }
+  validates :location, presence: true, length: { maximum: 256 }, uniqueness: { scope: :journey }
+  validates :latitude, presence: true,
+    :numericality => { :greater_than_or_equal_to => -90, :less_than_or_equal_to => 90 }
+  validates :longitude, presence: true,
+    :numericality => { :greater_than_or_equal_to => -180, :less_than_or_equal_to => 180 }
 
 end
 
