39de632d98febaab7c70887ded5d549eb9db79b5
Josh Fishbein <Josh@Joshs-MacBook-Pro.local>
2014-07-13 23:57:41 +1200

    Update production.rb to fix email sending through gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,13 +83,13 @@ TravelBlogger::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'http://journeytravelblog.herokuapp.com' }
-
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV['GMAIL_DOMAIN'],
+    domain: 'http://journeytravelblog.herokuapp.com' ,
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
